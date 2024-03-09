612f3944a91377ffd9f7e04fad8a1191c52fb436
silanurs <sarii.silanur@gmail.com>
2024-03-09 19:28:26 +0300

    code refactoring

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
 const http = require('node:http');
 const fs = require('node:fs');
 
+
 const server = http.createServer();
 server.on('request', (req, res)=>{
-  if(req.url==="/" || req.url==="/index"){
-    fs.readFile("index.html", (err,data)=>{
-      res.writeHead(200, {"Content-type": "text/html"})
-      res.write(data);
-      res.end()
-    })
-  } else if (req.url === "/about"){
-    fs.readFile("about.html", (err,data)=>{
-      res.writeHead(200, {"Content-type": "text/html"})
-      res.write(data);
-      res.end()
-    })
-  } else if (req.url==="/contact-me"){
-    fs.readFile("contact-me.html", (err,data)=>{
-      res.writeHead(200, {"Content-type": "text/html"})
-      res.write(data);
-      res.end()
-    })
-  } else {
-    fs.readFile("404.html", (err,data)=>{
-      res.writeHead(404, {"Content-type": "text/html"})
-      res.write(data);
-      res.end()
-    })
-  }
+  const fileName=req.url==="/" || req.url ==="/index" ? "index.html" : req.url==="/about" ? "about.html" : req.url==="/contact-me" ? "contact-me.html" : "404.html"
+   fs.readFile(fileName, (err,data) =>{
+    res.writeHead(200, {"Content-type": "text/html"})
+    res.write(data);
+    res.end()
+   })
 }
 )
 server.listen(8080)
